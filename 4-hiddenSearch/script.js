ad53c59b01956fee0b696e2e12bf513ab254ea1a
karlG <kallegunnarsson@hotmail.com>
2021-05-31 01:04:00 +0200

    ✨ 4 hidenSearch. Close via click outside & esc

--- 4-hiddenSearch/script.js
+++ 4-hiddenSearch/script.js
@@ -6,3 +6,27 @@ btn.addEventListener('click', () => {
     search.classList.toggle('active');
     input.focus();
 });
+
+// close search if user clicks outside while search is active
+document.addEventListener("click", (evt) => {
+    let targetElement = evt.target; // clicked element
+
+    do {
+        if (targetElement == search) {
+            // This is a click inside. Do nothing, just return.
+            return;
+        }
+        // Go up the DOM
+        targetElement = targetElement.parentNode;
+    } while (targetElement);
+
+    // This is a click outside.
+    search.classList.remove('active');
+});
+
+// close search if user presses esc while search is active
+document.addEventListener('keydown', (event) => {
+    if (event.key === "Escape" && search.classList.contains('active')) {
+        search.classList.remove('active');
+    }
+});
